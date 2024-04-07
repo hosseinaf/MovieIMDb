bf36176f534d4cf66f0c2fcfbff170d3dabd6a63
Hossein <hossein.aflaki70@gmail.com>
2024-04-07 19:17:21 +0330

    add about

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -4,6 +4,7 @@ import MenuItem from "./MenuItem";
 import { CiHome } from "react-icons/ci";
 import { BsFillInfoCircleFill } from "react-icons/bs";
 import Link from "next/link";
+import DarkModeSwitch from "./DarkModeSwitch";
 function Header() {
   return (
     <div className="flex justify-between items-center p-3 max-w-6xl mx-auto">
@@ -11,10 +12,14 @@ function Header() {
         <MenuItem title="Home" address="/" Icon={CiHome} />
         <MenuItem title="About" address="/about" Icon={BsFillInfoCircleFill} />
       </div>
+      <div className="flex items-center gap-4">
+        <DarkModeSwitch/>
       <Link href="/" className="flex gap-1 items-center">
         <span className="text-2xl font-bold bg-amber-500 py-1 px-2">IMDb</span>
         <span className="text-xl hidden sm:inline">Clone</span>
       </Link>
+      </div>
+     
     </div>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,9 @@
 import Image from "next/image";
-import About from './about'
-
+ 
 export default function Home() {
   return (
   <>
-  
+  home
   </>
     
   );
